ab71dfb1a385f0dbd3675c030f7ad5aee1d40f3d
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-12-01 16:05:19 -0500

    config mode development

---
+++ webpack.config.dev.js
@@ -0,0 +1,86 @@
+const path = require('path')
+//plugin html
+const HtmlWebpackPLugin = require('html-webpack-plugin')
+//css plugin
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+//copy files
+const CopyPLugin = require('copy-webpack-plugin')
+//variables de entorno
+const Dotenv = require('dotenv-webpack')
+
+/** @type {import('webpack').Configuration} */
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+    },
+    mode: 'development',
+    resolve: {
+        extensions: ['.mjs','.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+
+        }
+    },
+    module: {
+        rules: [
+            //babel
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            //css
+            {
+                test: /\.css|.styl$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            //images
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[hash][ext][query]'
+                }
+            },
+            //fonts
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name][ext]'
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPLugin({
+            inject: 'body', //true - head - false
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+        new CopyPLugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname,"src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        }),
+        new Dotenv()
+    ]
+}
